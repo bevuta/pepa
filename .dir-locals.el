abb282877b977ef13decff2760f2ce9c13a90d8b
Moritz Ulrich <moritz.ulrich@bevuta.com>
2015-01-08 15:48:17 +0100

    Add a .dir-locals file for correct(er) indentation.

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((clojure-mode
+  (eval put-clojure-indent 'match 1)
+  (eval put-clojure-indent 'record 1)
+  (eval put-clojure-indent 'let->> 1)))
+
