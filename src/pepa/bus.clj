20eb8c6ab4d6c61ae8a49dc723692850ff6d4a44
Moritz Ulrich <moritz@tarn-vedra.de>
2015-01-08 12:57:46 +0100

    Some documentation notes on pepa.bus.

--- src/pepa/bus.clj
+++ src/pepa/bus.clj
@@ -2,11 +2,21 @@
   (:require [com.stuartsierra.component :as component]
             [clojure.core.async :as async :refer [>!!]]))
 
-(defn subscribe [bus topic]
+;;; pepa.bus is a simple application-wide notification bus. Clients
+;;; can subscribe for any topic they want and will receive a value for
+;;; any cann of `notify!' for that topic.
+
+(defn subscribe
+  "Returns a channel with that will receive a value when `notify!' is
+  called for that topic."
+  [bus topic]
   (let [chan (async/chan (async/sliding-buffer 1))]
     (async/sub (:output bus) topic chan)
     chan))
 
+;;; TODO: Add optional extra data. Replace `identity' in `async/pub'
+;;; below with ::topic or similar and make `notify!' put maps into the
+;;; channel.
 (defn notify! [bus topic]
   (>!! (:input bus) topic ))
 
