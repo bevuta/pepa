b7737c6ff218f8e8bca60687244319db9893c081
Moritz Ulrich <moritz.ulrich@bevuta.com>
2015-01-15 11:05:23 +0100

    Improve search result handling.
    ...by canceling previous searches etc. etc.

--- src-cljs/pepa/search.cljs
+++ src-cljs/pepa/search.cljs
@@ -22,22 +22,56 @@
 (defn search-results [state]
   (:search/results state))
 
+(defn ^:private cancel-search! [state]
+  (when-let [ch (:search/channel state)]
+    (async/close! ch)
+    (println "Canceled previous search"
+             (str "(for " (pr-str (:search/query state)) ")"))
+    (om/update! state :search/channel nil)))
+
 (defn clear-results!
   "Resets search results to *all* documents on the server."
   [state]
   (go
-    (om/update! state :search/results
-                (<! (api/fetch-document-ids)))))
+    ;; NOTE: This is quite ugly. We should introduce a :search
+    ;; top-level key.
+    (when (let [query (:search/query state)]
+            (or query
+                (and (nil? query)
+                     (nil? (:search/channel state))
+                     (empty? (:search/results state)))))
+      (let [ch (api/fetch-document-ids)]
+        (doto state
+          (cancel-search!)
+          (om/update! :search/results nil)
+          (om/update! :search/query nil)
+          (om/update! :search/channel ch))
+        (when-let [results (<! ch)]
+          (doto state
+            (om/update! :search/channel nil)
+            (om/update! :search/results results)))))))
 
 (defn search! [state query]
   (go
-    (some->> (try
-               (cond 
-                 (list? query)
-                 query
-                 string?
-                 (parse-query query))
-               (catch js/Error e nil))
-             (api/search-documents)
-             (<!)
-             (om/update! state :search/results))))
+    (let [query (try
+                  (cond 
+                    (list? query)
+                    query
+                    (string? query)
+                    (parse-query query))
+                  (catch js/Error e nil))]
+      (when (not= query (:search/query state))
+        (println "got new query")
+        ;; Cancel old search by closing the result channel
+        (cancel-search! state)
+        ;; Start new search, store result channel in app-state
+        (let [chan (api/search-documents query)]
+          (om/transact! state (fn [state]
+                                (assoc state
+                                       :search/query query
+                                       :search/results nil
+                                       :search/channel chan)))
+          (when-let [results (<! chan)]
+            (doto state
+              (om/update! :search/channel nil)
+              (om/update! :search/results results))))))))
