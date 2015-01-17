7caee35e28ae6e0ed068c5c44b0a4104eba01dec
Moritz Ulrich <moritz.ulrich@bevuta.com>
2015-01-17 11:55:50 +0100

    Draggable: Only handle 'main' mouse button.

--- src-cljs/pepa/components/draggable.cljs
+++ src-cljs/pepa/components/draggable.cljs
@@ -8,31 +8,33 @@
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
 (defn ^:private resize-draggable-mouse-down [owner e]
-  ;; Attach event handlers to window
-  (let [on-move (fn [e]
-                  (let [{:keys [dragging? positions xs ys]} (om/get-state owner)]
-                    (when dragging?
-                      (let [pos [(.-clientX e) (.-clientY e)]
-                            [x y] pos]
-                        (when positions (async/put! positions pos))
-                        (when xs (async/put! xs x))
-                        (when ys (async/put! ys y))))))
-        on-up (fn [e]
-                (doseq [handler (om/get-state owner :handlers)]
-                  (bevent/unlisten-by-key handler))
-                (doto owner
-                  (om/set-state! :dragging? false)
-                  (om/set-state! :start nil)
-                  (om/set-state! :handlers nil)))]
-    (let [el js/window.document.documentElement
-          handlers [(bevent/listen el "mousemove" on-move)
-                    (bevent/listen el "mouseup" on-up)]]
-      (doto owner
-        (om/set-state! :dragging? true)
-        (om/set-state! :handlers handlers))))
-  (doto e
-    (.preventDefault)
-    (.stopPropagation)))
+  ;; Only handle 'main' mouse button
+  (when (zero? (.-button e))
+    ;; Attach event handlers to window
+    (let [on-move (fn [e]
+                    (let [{:keys [dragging? positions xs ys]} (om/get-state owner)]
+                      (when dragging?
+                        (let [pos [(.-clientX e) (.-clientY e)]
+                              [x y] pos]
+                          (when positions (async/put! positions pos))
+                          (when xs (async/put! xs x))
+                          (when ys (async/put! ys y))))))
+          on-up (fn [e]
+                  (doseq [handler (om/get-state owner :handlers)]
+                    (bevent/unlisten-by-key handler))
+                  (doto owner
+                    (om/set-state! :dragging? false)
+                    (om/set-state! :start nil)
+                    (om/set-state! :handlers nil)))]
+      (let [el js/window.document.documentElement
+            handlers [(bevent/listen el "mousemove" on-move)
+                      (bevent/listen el "mouseup" on-up)]]
+        (doto owner
+          (om/set-state! :dragging? true)
+          (om/set-state! :handlers handlers))))
+    (doto e
+      (.preventDefault)
+      (.stopPropagation))))
 
 (def right-align-xform
   (map #(- (.-width (gdom/getViewportSize)) %)))
